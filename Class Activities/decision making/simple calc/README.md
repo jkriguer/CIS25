48748d4c283c2cc25a38f584614732122139f03d
Jasper Kriguer <jasper.kriguer@outlook.com>
2025-01-29 13:27:32 -0800

    initial upload

---
+++ Class Activities/decision making/simple calc/README.md
@@ -0,0 +1,4 @@
+# Simple Calculator
+This program contains a function that can do some basic arithmetic using user input.
+
+Solution (.sln) file is optional, I use it for working on multiple machines.
---
+++ Class Activities/decision making/simple calc/main.cpp
@@ -0,0 +1,39 @@
+#include <iostream>
+
+using namespace std;
+
+int main() {
+	double operand1 = 0.0, operand2 = 0.0;
+	cout << "Enter both operands: ";
+	cin >> operand1 >> operand2;
+	char operation;
+	bool repeat = true;
+	while (repeat) {
+		cout << "Enter the operation to perform: (+ - * /): ";
+		cin >> operation;
+		switch (operation) {
+			case '-':
+				operand2 = -operand2;
+			case '+':
+				cout << operand1 + operand2 << '\n';
+				repeat = false;
+				break;
+			case '*':
+				cout << operand1 * operand2 << '\n';
+				repeat = false;
+				break;
+			case '/':
+				if (operand2 != 0)
+					cout << operand1 / operand2 << '\n';
+				else
+					cout << "Undefined\n";
+				repeat = false;
+				break;
+			default:
+				cout << '"' << operation << "\" is not recognized. Please enter a valid operation. ";
+		}
+	}
+
+
+	return 0;
+}
