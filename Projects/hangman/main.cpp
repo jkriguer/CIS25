aba34a4613f9987c741dcbd39f617669046174cd
Jasper Kriguer <jasper.kriguer@outlook.com>
2025-02-15 16:56:08 -0800

    removed debug elements

--- Projects/hangman/main.cpp
+++ Projects/hangman/main.cpp
@@ -8,35 +8,30 @@
 int main() {
 	using std::cout, std::string, std::vector;
 
-	const int NUM_WORDS = 3; //2 words will be selected
+	const int NUM_WORDS = 2; //2 words will be selected
 	vector<string> wordList;
 	int errorLevel = populateWordList(wordList); //initialize wordlist from file
 	if (errorLevel != 0) { //terminate early if there were problems generating wordlist
 		cout << "Error " << errorLevel << ", check the README for more information.\n";
 		return errorLevel;
 	}
+
 	//prepare vars
 	string words = pickFromVec(wordList, NUM_WORDS); //select word(s) from list
-	string message = "Welcome! Guess a letter.";
 	wordList.clear(); //wordlist can be freed to save memory
 	int lives = 7;
 	vector<char> guessedLetters;
-	cout << "Debug: " << words << '\n';
-	system("pause");
+
 	//game start
 	while (lives > 0 && lettersLeftToGuess(words, guessedLetters) > 0) { //run until word is guessed or man is hanged
-		drawBoard(lives, message, words, guessedLetters); //draw game state
+		drawBoard(lives, words, guessedLetters); //draw game state
 		char input = getValidLetter(guessedLetters); //get user input, trunc string if needed
 		guessedLetters.push_back(input); //store newly guessed letter
-		if (charInString(input, words)) { //guess was correct
-			message = "Good guess!";
-		}
-		else {
+		if (!charInString(input, words)) { //guess was incorrect
 			lives--;
-			message = "That wasn't right.";
 		}
 	}
-	drawBoard(lives, ((lives == 0) ? "Good try!" : "Well done!"), words, guessedLetters);//display final board
+	drawBoard(lives, words, guessedLetters);//display final board
 	cout << "Game over!\n";
 	cout << ((lives == 0) ? "The solution was \"" + words + "\"" : "You guessed all the words!") << '\n';
 	cout << "You made " << guessedLetters.size() << " guesses.\n";
