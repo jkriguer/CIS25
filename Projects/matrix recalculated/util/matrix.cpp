5a1ddd4ada5175ab6494e97818f5bc164304b259
Jasper Kriguer <jasper.kriguer@outlook.com>
2025-03-09 00:31:37 -0800

    removed excessive constructors

--- Projects/matrix recalculated/util/matrix.cpp
+++ Projects/matrix recalculated/util/matrix.cpp
@@ -1,15 +1,12 @@
 #include "matrix.h"
 
-Matrix::Matrix() { //2x2 ident
-	Matrix(2, 2);
-}
-Matrix::Matrix(int dim) : Matrix() { //x*x ident
+Matrix::Matrix(int dim) { //x*x ident
 	
 }
-Matrix::Matrix(int _rows, int _cols) : Matrix() { //x*y ident
+Matrix::Matrix(int _rows, int _cols) { //x*y all zero
 
 }
-Matrix::Matrix(int _rows, int _cols, vector<int>vec) { //x*y matrix with vec elements
+Matrix::Matrix(int _rows, int _cols, vector<int> vec) { //x*y matrix with vec elements
 	if (vec.size() != _rows * _cols) { //terminate if vec doesn't match, maybe output an error?
 
 	}
--- Projects/matrix recalculated/util/matrix.h
+++ Projects/matrix recalculated/util/matrix.h
@@ -8,12 +8,11 @@ class Matrix {
 private:
 	int rows;
 	int cols;
-	unique_ptr<vector<vector<int>>> contents; //wtf
+	unique_ptr<vector<vector<int>>> contents;
 public:
 	//constructors
-	Matrix(); //2*2 identity
 	Matrix(int); //x*x identity
-	Matrix(int, int); //x*y identity
+	Matrix(int, int); //x*y zeroes
 	Matrix(int, int, vector<int>); //input vec must be x*y elements long
 	Matrix(vector<vector<int>>); //matrix size determined by vec dimensions
 	//setters and getters
