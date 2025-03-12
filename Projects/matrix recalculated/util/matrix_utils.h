a4a0c34e2776e46adfc4b01c401f80e782bade42
Jasper Kriguer <jasper.kriguer@outlook.com>
2025-03-11 19:22:52 -0700

    reordered function list in header for readability

--- Projects/matrix recalculated/util/matrix_utils.h
+++ Projects/matrix recalculated/util/matrix_utils.h
@@ -10,11 +10,12 @@ Matrix identityMatrixAdd(int, int);
 Matrix identityMatrixAdd(Matrix);
 Matrix identityMatrixMult(int);
 Matrix inverseMatrixAdd(Matrix);
+Matrix makeMatrix(int, int, vector<double>); //vec must contain x*y elements
+
 Matrix matrixMathAdd(Matrix, Matrix); //both matrices must have same dimensions
 Matrix matrixMathSub(Matrix, Matrix); //both matrices must have same dimensions
 Matrix matrixMathMult(Matrix, Matrix); //matrix A cols and matrix B rows must be equal
 
-Matrix makeMatrix(int, int, vector<double>); //vec must contain x*y elements
 bool isMatrixValid(Matrix);
 bool canMatricesAdd(Matrix, Matrix);
 bool canMatricesMult(Matrix, Matrix);
