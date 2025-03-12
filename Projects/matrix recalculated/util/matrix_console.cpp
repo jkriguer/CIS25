d35e766d562fb7532fd4592337a364c7b339be8f
Jasper Kriguer <jasper.kriguer@outlook.com>
2025-03-11 19:22:18 -0700

    fixed inaccurate var name

--- Projects/matrix recalculated/util/matrix_console.cpp
+++ Projects/matrix recalculated/util/matrix_console.cpp
@@ -11,16 +11,16 @@ void inputMatrix(Matrix& mat) {
 
 void printMatrix(Matrix mat) {
 	int width = 3; //arbitrary start width
-	for (const auto& col : mat) { //first loop for spacing width
-		for (double item : col) {
+	for (const auto& row : mat) { //first loop for spacing width
+		for (double item : row) {
 			int newWidth = std::format("{:g}", item).length() + 1; //convert to string and remove padding
 			if (newWidth > width) { //store new width if wider than current widest
 				width = newWidth;
 			}
 		}
 	}
-	for (const auto& col : mat) { //second loop for printing
-		for (double item : col) {
+	for (const auto& row : mat) { //second loop for printing
+		for (double item : row) {
 			std::cout << std::setw(width) << std::left << std::format("{:g}", item);
 		}
 		std::cout << '\n';
