d17fcd6db38280ced39e9bb1f24cefc3e53feed5
Jasper Kriguer <jasper.kriguer@outlook.com>
2025-04-23 06:20:26 -0700

    new header file for types used throughout project

---
+++ Projects/sam-i-am/utils/sam_types.h
@@ -0,0 +1,17 @@
+#ifndef SAM_TYPES
+#define SAM_TYPES
+
+#include <vector>
+#include <memory>
+#include "Actor.h" //Actor class
+
+class Actor; //forward dec
+
+using Board = std::vector<std::vector<std::unique_ptr<Actor>>>; //saves so much space
+using SharedBoard = std::shared_ptr<Board>;
+
+enum class Bearing { N = 8, E = 6, S = 2, W = 4, NW = 7, NE = 9, SE = 3, SW = 1 }; //numpad
+enum class Faction { NEUTRAL, HOSTILE, FRIENDLY, UNKNOWN };
+
+
+#endif // !SAM_TYPES
