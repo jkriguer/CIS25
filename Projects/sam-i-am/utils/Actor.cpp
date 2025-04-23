fa04cd95bf72f21a63a01658e4615a4681f4b1a5
Jasper Kriguer <jasper.kriguer@outlook.com>
2025-04-23 06:21:51 -0700

    nonfunctional refactor of Actor class

--- Projects/sam-i-am/utils/Actor.cpp
+++ Projects/sam-i-am/utils/Actor.cpp
@@ -1,1 +1,11 @@
-#include "Actor.h"
+#include "Actor.h" //header
+#include "sam_utils.h" //getContactNumber
+
+//constructors
+//Actor::Actor() : board(nullptr) {}; //noarg
+Actor::Actor(const SharedBoard& b, int _xCoord, int _yCoord, std::string _label) : board(b) { //args
+	this->xCoord = _xCoord;
+	this->yCoord = _yCoord;
+	this->label = _label;
+	this->contact = getContactNumber();
+}
--- Projects/sam-i-am/utils/Actor.h
+++ Projects/sam-i-am/utils/Actor.h
@@ -1,12 +1,23 @@
 #ifndef ACTOR_CLASS
 #define ACTOR_CLASS
 
-enum class Faction { NEUTRAL, HOSTILE, FRIENDLY, UNKNOWN };
+#include "sam_types.h" //types
+#include <string>
+
 
 class Actor {
-private:
-	//std::string label;
+protected:
+	const SharedBoard board;
+	int xCoord = 0;
+	int yCoord = 0;
+	std::string label = "Unnamed Actor";
+	int contact;
 public:
+	//constructors
+	//Actor();
+	Actor(const SharedBoard&, int, int, std::string);
+	virtual ~Actor() = default; //deconstructor for subclass(es?)
+
 };
 
 #endif // !ACTOR
