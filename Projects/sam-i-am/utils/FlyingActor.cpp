e5496facd6684b9e49490c9113c8700288676ed9
Jasper Kriguer <jasper.kriguer@outlook.com>
2025-04-23 06:22:22 -0700

    FA class NYI, heavily commented and placeholder for now

--- Projects/sam-i-am/utils/FlyingActor.cpp
+++ Projects/sam-i-am/utils/FlyingActor.cpp
@@ -1,26 +1,29 @@
-#include "FlyingActor.h"
+#include "FlyingActor.h" //header
 
 //constructors
-FlyingActor::FlyingActor(std::string _label, Faction _faction, double _factionConfidence, int _speed, Bearing _bearing, bool _flyingLow) 
-{
-	this->label = _label;
-	this->factionActual = _faction;
-}
-
-//other methods
-bool FlyingActor::isHot() { //returns true if flying towards player
-	return true; //NYI
-}
-
-std::string FlyingActor::toString() {
-	//return "Label: " + getLabel() +
-	//	"\nBRAA: " + readBRAA();
-	return "NYI";
-}
-
-std::string FlyingActor::readBRAA() {
-	return "Bearing: NYI";
-}
+//FlyingActor::FlyingActor() {
+//	base(nullptr, 0, 0, "label");
+//}
+//FlyingActor::FlyingActor(std::string _label, Faction _faction, double _factionConfidence, int _speed, Bearing _bearing, bool _flyingLow) 
+//{
+//	base(nullptr, 1, 1, "Test");
+//	this->label = _label;
+//}
+//
+////other methods
+//bool FlyingActor::isHot() { //returns true if flying towards player
+//	return true; //NYI
+//}
+//
+//std::string FlyingActor::toString() {
+//	//return "Label: " + getLabel() +
+//	//	"\nBRAA: " + readBRAA();
+//	return "NYI";
+//}
+//
+//std::string FlyingActor::readBRAA() {
+//	return "Bearing: NYI";
+//}
 
 //std::ostream& operator<<(std::ostream& os, const FlyingObject& fo) { //NYI
 //	os << fo.toString();
--- Projects/sam-i-am/utils/FlyingActor.h
+++ Projects/sam-i-am/utils/FlyingActor.h
@@ -4,29 +4,26 @@
 #include <iostream>
 #include <string>
 #include "Actor.h"
-
-enum class Bearing { N = 8, E = 6, S = 2, W = 4, NW = 7, NE = 9, SE = 3, SW = 1 };
-
+#include "sam_types.h"
 
 class FlyingActor : public Actor {
 private:
 	typedef Actor base; //*super* cool!
-	std::string label; //the contact number/any needed info for listing the contact
-	Faction factionActual;
-	Faction factionDisplay = Faction::UNKNOWN;
+	Faction faction;
 	double factionConfidence; //from 0.0 (unidentified) to 1.0 (fully identified)
 	int speed; //in tiles per turn
 	Bearing bearing;
 	bool flyingLow;
 public:
+	FlyingActor();
 	//constructors
-	FlyingActor(std::string _label, Faction _faction, double _factionConfidence, int _speed, Bearing _bearing, bool _flyingLow);
+	//FlyingActor(std::string _label, Faction _faction, double _factionConfidence, int _speed, Bearing _bearing, bool _flyingLow);
 	//setters and getters
 	
-	//other methods
-	bool isHot();
-	std::string readBRAA();
-	std::string toString();
+	////other methods
+	//bool isHot();
+	//std::string readBRAA();
+	//std::string toString();
 };
 
 //std::ostream& operator<<(std::ostream&, const FlyingObject&);
