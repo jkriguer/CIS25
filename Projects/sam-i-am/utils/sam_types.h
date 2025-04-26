bf234849d6270b3ef9d8aaabdadde02e6a6a4df0
Jasper Kriguer <jasper.kriguer@outlook.com>
2025-04-25 19:28:07 -0700

    bearing simplified

--- Projects/sam-i-am/utils/sam_types.h
+++ Projects/sam-i-am/utils/sam_types.h
@@ -9,8 +9,8 @@ class Actor; //forward dec
 using Board = std::vector<std::vector<std::unique_ptr<Actor>>>; //saves so much space
 using SharedBoard = std::shared_ptr<Board>;
 
-enum class Bearing { N = 8, E = 6, S = 2, W = 4, NW = 7, NE = 9, SE = 3, SW = 1 }; //numpad
-enum class Faction { NEUTRAL, HOSTILE, FRIENDLY, UNKNOWN };
+enum Bearing { N, E, S, W, NW, NE, SE, SW };
+enum Faction { NEUTRAL, HOSTILE, FRIENDLY, UNKNOWN };
 
 
 #endif // !SAM_TYPES
