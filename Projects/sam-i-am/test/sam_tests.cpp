7eaa60ddc999d0f8db9fb352098ad6103e908a40
Jasper Kriguer <jasper.kriguer@outlook.com>
2025-05-21 01:55:16 -0700

    new fixture tests added

--- Projects/sam-i-am/test/sam_tests.cpp
+++ Projects/sam-i-am/test/sam_tests.cpp
@@ -1,5 +1,6 @@
 // tests/sam_unit_tests.cpp
 #include <gtest/gtest.h>
+#include <vector>
 #include "../include/sam_io.h"
 #include "../include/sam_types.h"
 #include "../include/sam_utils.h"
@@ -7,13 +8,13 @@
 #include "../include/StaticActor.h"
 #include "../include/MovingActor.h"
 
-TEST(Utils, manhattanDistance) { //simple math
+TEST(Utils, ManhattanDistance) { //simple math
 	EXPECT_EQ(SAM::manhattan({ 0, 0 }, { 0, 0 }), 0);
 	EXPECT_EQ(SAM::manhattan({ 1, 2 }, { 4, 6 }), 7);
 	EXPECT_EQ(SAM::manhattan({ -3, 5 }, { -1, 1 }), 6);
 }
 
-TEST(Utils, coordToStr) {
+TEST(Utils, CoordToStr) {
 	EXPECT_EQ(SAM::coordToStr({ 0,0 }), "(1, 1)");
 	EXPECT_EQ(SAM::coordToStr({ 4,9 }), "(5, 10)");
 }
@@ -26,7 +27,7 @@ protected:
 	}
 };
 
-TEST_F(GameFixture, boundsChecking) {
+TEST_F(GameFixture, BoundsChecking) {
 	EXPECT_TRUE(g.inBounds({ 0, 0 }));
 	EXPECT_TRUE(g.inBounds({ SAM::MAX_X - 1, SAM::MAX_Y - 1 }));
 	EXPECT_FALSE(g.inBounds({ -1, 0 }));
@@ -34,12 +35,12 @@ TEST_F(GameFixture, boundsChecking) {
 	EXPECT_FALSE(g.inBounds({ 0, SAM::MAX_Y }));
 }
 
-TEST_F(GameFixture, makeAndPlaceOverwrite) {
+TEST_F(GameFixture, MakeAndPlaceOverwrite) {
 	EXPECT_FALSE(g.makeAndPlaceCity({ 0, 0 })); //should be unable to overwrite
 	EXPECT_EQ(g.getCell({ 0,0 })->getMapIcon(), 'B'); //should match original
 }
 
-TEST_F(GameFixture, missileLaunch) {
+TEST_F(GameFixture, MissileLaunch) {
 	g.makeAndPlaceCity({ 2, 2 });
 	g.makeAndPlaceMoving(Enemy, SAM::getArchetype(Enemy, 0), North, { 4, 4 }); //within range
 	EXPECT_TRUE(g.launchMissile({ 4, 4 })); //should hit, in range
@@ -48,14 +49,14 @@ TEST_F(GameFixture, missileLaunch) {
 	EXPECT_FALSE(g.launchMissile({ 25, 25 })); //should miss without target
 }
 
-TEST_F(GameFixture, actorLeavesOOB) {
+TEST_F(GameFixture, ActorLeavesOOB) {
 	AircraftParams ap = { "Test", 100 }; //test aircraft that should leave bounds in 1 turn
 	g.makeAndPlaceMoving(Enemy, ap, North, { 10, 10 });
 	g.moveUnits({ {10, 10} });//should immediately be deleted
 	EXPECT_FALSE(g.getCell({ 10, 10 })); //source cell should be empty
 }
 
-TEST_F(GameFixture, sortByDistance) {
+TEST_F(GameFixture, SortByDistance) {
 	Coord a1{ 2, 2 }; //4 away
 	Coord a2{ 1, 0 }; //1 away
 	Coord a3{ 3, 0 }; //3 away
@@ -69,10 +70,39 @@ TEST_F(GameFixture, sortByDistance) {
 	EXPECT_EQ(vec[2].x, a1.x);
 }
 
-TEST(Actor, identifiedBool) {
+TEST_F(GameFixture, LoadScenarioBadValidator) {
+	std::vector<char> badScenario{
+		(char)(SAM::SAVE_VERSION + 1), //bad validator
+		2, //2 static actors
+		0, 0, 0, //player at 0,0
+		2, 1, 1, //city at 1, 1
+		0, //zero mobile actors
+		(char)-SAM::SAVE_VERSION //correct exit validator
+	};
+	EXPECT_THROW(g.loadScenario(badScenario), std::runtime_error);
+}
+
+TEST_F(GameFixture, LoadScenarioNoPlayer) {
+	std::vector<char> badScenario{
+		(char)SAM::SAVE_VERSION, //good validator
+		1, //1 static actor
+		2, 0, 0, //city at 0, 0
+		0, //zero mobile actors
+		(char)-SAM::SAVE_VERSION //good exit validator
+	};
+	EXPECT_THROW(g.loadScenario(badScenario), std::runtime_error);
+}
+
+TEST_F(GameFixture, IdentifyAction) {
+	Coord c{ 5, 5 };
+	g.makeAndPlaceMoving(Enemy, { "Bogey", 1, true, true }, North, c);
+	EXPECT_TRUE(g.identify(c)); //should succeed, bogey is now IDed
+	EXPECT_FALSE(g.identify(c)); //should fail, bogey is already IDed
+}
+
+TEST(Actor, IdentifiedBool) {
 	SAM::MovingActor aircraftHostile(Enemy, SAM::getArchetype(Enemy, 0), Bearing::North); //enemy bomber
 	SAM::MovingActor aircraftFriendly(Friendly, SAM::getArchetype(Friendly, 0), Bearing::North); //friendly fighter
 	EXPECT_FALSE(aircraftHostile.isIdentified);
 	EXPECT_TRUE(aircraftFriendly.isIdentified);
 }
-
